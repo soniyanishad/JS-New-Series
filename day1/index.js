d5dce3860f1fc2d50ff482518ea6895d5df54a6e
Soniya Nishad <soniyanishad10@gmail.com>
2025-05-02 12:23:01 +0530

    Add files via upload

---
+++ day1/index.js
@@ -0,0 +1,18 @@
+console.log("Hello World!");
+
+
+
+ var data="hii";
+ var data="hello";
+    console.log(data);
+
+let name1 = "John Doe";
+ name1="Micheal Smith";
+ console.log(name1);
+
+ const name2 = "John Doe";
+ name2="Micheal Smith";
+
+console.log(name2);
+
+
