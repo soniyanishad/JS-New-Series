5a32e4a210aea220a820d8dd785175d63abec509
Soniya Nishad <soniyanishad10@gmail.com>
2025-05-02 23:24:18 +0530

    Add files via upload

---
+++ day3/index.js
@@ -0,0 +1,38 @@
+
+
+//Check the year is century year or not
+
+// let year= prompt("Enter a year: ");
+// if(year%100===0)
+// {
+//     console.log("year is century year");
+    
+// }else {
+//     console.log("year is not century year");
+// }
+
+
+//Check in the end of number wheather it has 0 or not
+
+
+// let num= prompt("Enter a number: ");
+// if(num%10===0){
+//     console.log("Number has o in the end");
+    
+// }
+// else{
+//     console.log("Number has not 0 in the end");
+    
+// }
+
+let num=7;
+
+ if(num%2==0)
+{
+    console.log("Number is prime");
+}
+else{
+    console.log("Number is not prime");
+}
+
+
